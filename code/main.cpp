b267647876d29bc71778b94c4ab6b62d31a0ad7e
Borges <sdafdsf@.asdsa>
2023-10-30 07:49:56 -0300

    adding type alias declarations

--- code/main.cpp
+++ code/main.cpp
@@ -1,127 +1,127 @@
-//#include <iostream>
-//#include <cmath>
-//#include <cstdlib>
-//#include <ctime>
-//
-//using namespace std;
-//
-//int main() {
-//
-///*
-// *
-// * << insertion stream operator
-// * >> extraction stream operator
-// *
-// *
-//    int x  = 10;
-//    int z = x++; // z = 10 - x = 11
-//    int y = ++x; // y = 11 - x = 11
-//
-//
-//
-//    double x = 10;
-//    double y = 5;
-//
-//    double z = (x + 10) /  (3 * y);
-//
-//    std::cout << z;
-//
-//
-//    int x = 10;
-//
-//    cout << "x = " << x << endl;
-//    cout << "x = " << x << endl;
-//
-//    const double stateTax = .04;
-//    const double countyTax = .02;
-//    double sales = 95000;
-//
-//    double paidStateTax = sales * stateTax;
-//    double paidCountyTax = sales * countyTax;
-//
-//    double totalPaidTax = paidStateTax + paidCountyTax;
-//
-//    double totalSales = sales - totalPaidTax;
-//
-//    cout << "state tax = " << paidStateTax << endl;
-//    cout << "county tax = " << paidCountyTax << endl;
-//    cout << "total paid in tax = " << totalPaidTax << endl;
-//    cout << "final revenue = " << totalSales;
-//
-//    cout << "Enter values for x and y:";
-//
-//    int x;
-//    int y;
-//
-//    cin >> x >> y;
-//
-//    cout << x + y;
-//
-//    cout << "Enter the temperature in F";
-//
-//    double fahrenheit;
-//
-//    cin >> fahrenheit;
-//
-//    double celsius = (fahrenheit-32) / 1.8;
-//
-//    cout << "result = " << celsius;
-//
-//    double result = pow(2, 3);
-//
-//    cout << result;
-//
-//    // Area of a circle
-//
-//    cout << "Enter a radius";
-//
-//    int r;
-//
-//    cin >> r;
-//
-//    const double PI = 3.14;
-//
-//    double area = PI * pow(r, 2);
-//
-//    cout << area;
-//
-//    double a = 99.99;
-//    float b = 99.9F;
-//    long c = 900000L;
-//    char d = 'd';
-//    bool e = false;
-//    auto f = true;
-//    auto g = 'a';
-//
-//    int binaryNumber = 0b11111111;
-//    int hexadecimalNumber = 0xFF;
-//
-//    cout << binaryNumber << endl;
-//    cout << hexadecimalNumber;
-//
-//
-//    unsigned int number = -255;
-//
-//    cout << number;
-//
-//    srand(time(nullptr));
-//
-//    int number = rand();
-//
-//    cout << number;
-//
-//    // Roll dice
-//
-//    const short maxValue = 6;
-//    const short minValue = 1;
-//
-//    srand(time(nullptr));
-//
-//    short first = (rand() % (maxValue - minValue + 1)) + minValue;
-//    short second = (rand() % (maxValue - minValue + 1)) + minValue;
-//
-//    cout << first << ", " << second;
-//*/
-//
-//    return 0;
-//}
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+
+/*
+ *
+ * << insertion stream operator
+ * >> extraction stream operator
+ *
+ *
+    int x  = 10;
+    int z = x++; // z = 10 - x = 11
+    int y = ++x; // y = 11 - x = 11
+
+
+
+    double x = 10;
+    double y = 5;
+
+    double z = (x + 10) /  (3 * y);
+
+    std::cout << z;
+
+
+    int x = 10;
+
+    cout << "x = " << x << endl;
+    cout << "x = " << x << endl;
+
+    const double stateTax = .04;
+    const double countyTax = .02;
+    double sales = 95000;
+
+    double paidStateTax = sales * stateTax;
+    double paidCountyTax = sales * countyTax;
+
+    double totalPaidTax = paidStateTax + paidCountyTax;
+
+    double totalSales = sales - totalPaidTax;
+
+    cout << "state tax = " << paidStateTax << endl;
+    cout << "county tax = " << paidCountyTax << endl;
+    cout << "total paid in tax = " << totalPaidTax << endl;
+    cout << "final revenue = " << totalSales;
+
+    cout << "Enter values for x and y:";
+
+    int x;
+    int y;
+
+    cin >> x >> y;
+
+    cout << x + y;
+
+    cout << "Enter the temperature in F";
+
+    double fahrenheit;
+
+    cin >> fahrenheit;
+
+    double celsius = (fahrenheit-32) / 1.8;
+
+    cout << "result = " << celsius;
+
+    double result = pow(2, 3);
+
+    cout << result;
+
+    // Area of a circle
+
+    cout << "Enter a radius";
+
+    int r;
+
+    cin >> r;
+
+    const double PI = 3.14;
+
+    double area = PI * pow(r, 2);
+
+    cout << area;
+
+    double a = 99.99;
+    float b = 99.9F;
+    long c = 900000L;
+    char d = 'd';
+    bool e = false;
+    auto f = true;
+    auto g = 'a';
+
+    int binaryNumber = 0b11111111;
+    int hexadecimalNumber = 0xFF;
+
+    cout << binaryNumber << endl;
+    cout << hexadecimalNumber;
+
+
+    unsigned int number = -255;
+
+    cout << number;
+
+    srand(time(nullptr));
+
+    int number = rand();
+
+    cout << number;
+
+    // Roll dice
+
+    const short maxValue = 6;
+    const short minValue = 1;
+
+    srand(time(nullptr));
+
+    short first = (rand() % (maxValue - minValue + 1)) + minValue;
+    short second = (rand() % (maxValue - minValue + 1)) + minValue;
+
+    cout << first << ", " << second;
+*/
+
+    return 0;
+}
---
+++ code/typeDefDeclaration.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+//typedef std::vector<std::pair<std::string, int>> pairList_t;
+        // OR
+using pairList_t = std::vector<std::pair<std::string, int>>;
+
+int ble(){
+
+    pairList_t pairList;
+
+    return 0;
+}
